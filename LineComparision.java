17a2ab4849666ef9c8abf2542353617e1bdf7530
Bathala-Haresh <hareshbathala87@gmail.com>
2021-07-01 22:55:51 +0530

    program to calculate length of a line

--- LineComparision.java
+++ LineComparision.java
@@ -1,12 +1,38 @@
+
 /**
-* This program is welcome to the line Comparision Problem
+* This program is to calculate the Length of a Line
 *
 * @author  Bathala Haresh
 */
+import java.util.Scanner;
+public class LineComparision {
 
-public class LineComparision
-{
-        public static void main(String[] args) {
-                System.out.println("Welcome to Line Comparision Problem ");
-        }
-}
+	        public static void main(String args[])
+	        {
+	        	//variables
+	            int x1,y1,x2,y2;
+	            double length;
+	            
+	            Scanner sc=new Scanner(System.in);
+	            System.out.println("enter line coOrdinates  ");
+	            
+	            System.out.print("x1 : ");
+	            x1=sc.nextInt();
+	            
+	            System.out.print("x2 : ");
+	            x2=sc.nextInt();
+	            
+	            System.out.print("y1 : ");
+	            y1=sc.nextInt();
+	            
+	            System.out.print("y2 : ");
+	            y2=sc.nextInt();
+	            
+	            //Formula to calculate the length of a line
+	            length=Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+	            
+	            System.out.print("length between two lines is : " +length);
+	        }
+	    }
+
+
