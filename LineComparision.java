adcce88bb4a3258a6a2aea3c783b3a9c8912fbb5
Bathala-Haresh <hareshbathala87@gmail.com>
2021-07-01 22:48:54 +0530

    welcome to LineComparision

---
+++ LineComparision.java
@@ -0,0 +1,12 @@
+/**
+* This program is welcome to the line Comparision Problem
+*
+* @author  Bathala Haresh
+*/
+
+public class LineComparision
+{
+        public static void main(String[] args) {
+                System.out.println("Welcome to Line Comparision Problem ");
+        }
+}
